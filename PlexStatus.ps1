e4e4e8d26c48b9573a8922792d9ecebd7aaad426
Andreas Fenz <45659314+FSCorrupt@users.noreply.github.com>
2020-09-23 15:00:46 +0200

    Add files via upload

---
+++ PlexStatus.ps1
@@ -0,0 +1,28 @@
+﻿# Discord Webhook Uri
+$Uri = 'https://discordapp.com/api/webhooks/XXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+$StatusPage = 'https://status.plex.tv'
+$Loop = $null
+
+while ($Loop -ne 'Ended'){
+
+$WebRequest = Invoke-WebRequest -Uri $StatusPage
+$Status = ($WebRequest.AllElements | ? { $_.Class -eq 'status font-large' } | select innerText).innertext
+$Incident = ($WebRequest.AllElements | ? { $_.Class -eq 'incident-title font-large' } | select innerText).innertext
+
+if ($Status -match 'All Systems Operational' ){
+  $Content = '```CSS'+"`n"+"-"+$Status+"`n"+'```'
+}
+Else{
+  $Content = '```DIFF'+"`n"+"-"+$Incident+"`n"+'```'
+}
+
+$DCContent = @"
+  **Current status of [plex.tv](https://status.plex.tv):**$Content
+"@
+
+#Send to Discord
+$UserPayload = [PSCustomObject]@{content = $DCContent}
+Invoke-RestMethod -Uri $uri -Body ($UserPayload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'Application/Json'
+
+sleep 60
+}
