832a4c38854e6a41599a00cdecf80a78159012fb
Andreas Fenz <45659314+FSCorrupt@users.noreply.github.com>
2020-09-24 08:50:33 +0200

    Update PlexStatus.ps1
    It will now send on script start the current status into discord channel, but then only updates when status is Bad, so we dont get spamed if Status is OK.

--- PlexStatus.ps1
+++ PlexStatus.ps1
@@ -3,26 +3,55 @@ $Uri = 'https://discordapp.com/api/webhooks/XXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 $StatusPage = 'https://status.plex.tv'
 $Loop = $null
 
-while ($Loop -ne 'Ended'){
-
+# First Status Call
 $WebRequest = Invoke-WebRequest -Uri $StatusPage
 $Status = ($WebRequest.AllElements | ? { $_.Class -eq 'status font-large' } | select innerText).innertext
 $Incident = ($WebRequest.AllElements | ? { $_.Class -eq 'incident-title font-large' } | select innerText).innertext
 
-if ($Status -match 'All Systems Operational' ){
-  $Content = '```CSS'+"`n"+"-"+$Status+"`n"+'```'
-}
-Else{
-  $Content = '```DIFF'+"`n"+"-"+$Incident+"`n"+'```'
-}
+  if ($Status -match 'All Systems Operational' ){
+    $Content = '```CSS'+"`n"+"-"+$Status+"`n"+'```'
+    $Status = "Good"
+  }
+  Else{
+    $Content = '```DIFF'+"`n"+"-"+$Incident+"`n"+'```'
+    $Status = "Bad"
+  }
 
-$DCContent = @"
+  $DCContent = @"
   **Current status of [plex.tv](https://status.plex.tv):**$Content
 "@
 
-#Send to Discord
+#Send to Discord, each time the Script is starting
 $UserPayload = [PSCustomObject]@{content = $DCContent}
 Invoke-RestMethod -Uri $uri -Body ($UserPayload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'Application/Json'
 
-sleep 60
+
+# Loop for status.plex.tv, only posts to channel when status is 'Bad'
+
+while ($Loop -ne 'Ended'){
+
+  $WebRequest = Invoke-WebRequest -Uri $StatusPage
+  $Status = ($WebRequest.AllElements | ? { $_.Class -eq 'status font-large' } | select innerText).innertext
+  $Incident = ($WebRequest.AllElements | ? { $_.Class -eq 'incident-title font-large' } | select innerText).innertext
+
+  if ($Status -match 'All Systems Operational' ){
+    $Content = '```CSS'+"`n"+"-"+$Status+"`n"+'```'
+    $Status = "Good"
+  }
+  Else{
+    $Content = '```DIFF'+"`n"+"-"+$Incident+"`n"+'```'
+    $Status = "Bad"
+  }
+
+  $DCContent = @"
+  **Current status of [plex.tv](https://status.plex.tv):**$Content
+"@
+
+  if ($Status -eq 'Bad'){
+    #Send to Discord
+    $UserPayload = [PSCustomObject]@{content = $DCContent}
+    Invoke-RestMethod -Uri $uri -Body ($UserPayload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'Application/Json'
+  }
+  sleep 60
+  
 }
