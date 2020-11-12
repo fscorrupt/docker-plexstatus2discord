95151793471423284de5473b051cac09ff964994
Andreas Fenz <45659314+FSCorrupt@users.noreply.github.com>
2020-11-12 07:10:14 +0100

    Update PlexStatus.ps1
    Added a new Class, because of latest Status Error, 'page-status status-minor' and the ability to write error messages to a seperate Channel "AnnouncementUri".

--- PlexStatus.ps1
+++ PlexStatus.ps1
@@ -1,12 +1,13 @@
-﻿# Discord Webhook Uri
-$Uri = 'https://discordapp.com/api/webhooks/XXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+# Discord Webhook Uri
+$Uri = 'https://discordapp.com/api/webhooks/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+$AnnouncementUri = 'https://discordapp.com/api/webhooks/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 $StatusPage = 'https://status.plex.tv'
 $Loop = $null
 
 # First Status Call
 $WebRequest = Microsoft.PowerShell.Utility\Invoke-WebRequest -Uri $StatusPage
 $Status = ($WebRequest.AllElements | ? { $_.Class -eq 'status font-large' } | select innerText).innertext
-$Incident = ($WebRequest.AllElements | ? { $_.Class -eq 'incident-title font-large' } | select innerText).innertext
+$Incident = ($WebRequest.AllElements | ? { ($_.Class -eq 'incident-title font-large') -or ($_.class -eq 'page-status status-major') -or ($_.class -eq 'page-status status-minor')  } | select innerText).innertext
 
   if ($Status -match 'All Systems Operational' ){
     $Content = '```CSS'+"`n"+"-"+$Status+"`n"+'```'
@@ -30,7 +31,7 @@ while ($Loop -ne 'Ended'){
 
   $WebRequest = Microsoft.PowerShell.Utility\Invoke-WebRequest -Uri $StatusPage
   $Status = ($WebRequest.AllElements | ? { $_.Class -eq 'status font-large' } | select innerText).innertext
-  $Incident = ($WebRequest.AllElements | ? { $_.Class -eq 'incident-title font-large' } | select innerText).innertext
+  $Incident = ($WebRequest.AllElements | ? { ($_.Class -eq 'incident-title font-large') -or ($_.class -eq 'page-status status-major')  -or ($_.class -eq 'page-status status-minor') } | select innerText).innertext
 
   if ($Status -match 'All Systems Operational' ){
     $Content = '```CSS'+"`n"+"-"+$Status+"`n"+'```'
@@ -41,24 +42,39 @@ while ($Loop -ne 'Ended'){
     $Stat = "Bad"
   }
 
-  $DCContent = @"
+
+          $DCContent = @"
   **Current status of [plex.tv](https://status.plex.tv):**$Content
+"@
+          $ADCContent = @"
+ **Current status of [plex.tv](https://status.plex.tv):**$Content
 "@
 
   if ($Stat -eq 'Bad'){
+
+          #Send to Discord Announcement Channel
+          $AUserPayload = [PSCustomObject]@{content = $ADCContent}
+          Invoke-RestMethod -Uri $AnnouncementUri -Body ($AUserPayload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'Application/Json'
     do {
         $WebRequest = Microsoft.PowerShell.Utility\Invoke-WebRequest -Uri $StatusPage
         $Status = ($WebRequest.AllElements | ? { $_.Class -eq 'status font-large' } | select innerText).innertext
-        $Incident = ($WebRequest.AllElements | ? { $_.Class -eq 'incident-title font-large' } | select innerText).innertext
+        $Incident = ($WebRequest.AllElements | ? { ($_.Class -eq 'incident-title font-large') -or ($_.class -eq 'page-status status-major')  -or ($_.class -eq 'page-status status-minor') } | select innerText).innertext
 
         if ($Status -match 'All Systems Operational' ){
           $Content = '```CSS'+"`n"+"-"+$Status+"`n"+'```'
           $DCContent = @"
   **Current status of [plex.tv](https://status.plex.tv):**$Content
 "@
+          $ADCContent = @"
+ **Current status of [plex.tv](https://status.plex.tv):**$Content
+"@
           #Send to Discord
           $UserPayload = [PSCustomObject]@{content = $DCContent}
           Invoke-RestMethod -Uri $uri -Body ($UserPayload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'Application/Json'
+
+          #Send to Discord Announcement Channel
+          $AUserPayload = [PSCustomObject]@{content = $ADCContent}
+          Invoke-RestMethod -Uri $AnnouncementUri -Body ($AUserPayload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'Application/Json'
           $Stat = "Good"
         }
         Else{
